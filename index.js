7f144a302ba0e457bbd0b92015adb0244b3ef2cb
Barbara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-06-16 14:39:54 +0100

    adds route for bike requests

---
+++ index.js
@@ -0,0 +1,46 @@
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+const inert = require('inert');
+const vision = require('vision');
+const handlebars = require('handlebars');
+
+const fs = require('fs');
+
+const tls = {
+  key: fs.readFileSync('keys/key.pem'),
+  cert: fs.readFileSync('keys/cert.pem')
+};
+
+server.connection({address: '0.0.0.0', port: 8080, tls: tls });
+
+const routes = [
+    // is an object
+    require('./home'),
+    require('./get_bikes'),
+    {
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+      directory: {
+        path: './public'
+      }
+    }
+  }];
+
+
+server.register([inert, vision], (err) => {
+  if (err) console.log(err);
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: '.'
+  });
+
+  server.route(routes);
+});
+
+server.start(function () {
+    console.log(`Server running at port ${server.info.uri}`);
+});
