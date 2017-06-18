6586c1b9c39d5ca68f69dd754f2f2fab54f0c978
Nick Field <heyimnick@live.com>
2017-06-18 13:06:04 +0100

    move weather.js to app.js, refactor for ES5

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,105 @@
-const load = (data) => {
-  const fetched = document.getElementById("fetched");
-  fetched.classList.remove("hidden")
-  fetched.classList.add("visible")
-  fetched.scrollIntoView({ behavior: 'smooth' })
+(function(){
 
-  const rootElement = document.getElementById("bike-points")
+  var byId     = function(x){return document.getElementById(x)},
+      elt      = function(x){return document.createElement(x)},
+      geo      = function(x){return navigator.geolocation.getCurrentPosition(x)},
+      rain_key = "rain",
+      pol_key  = "polution";
 
-  data.forEach(point => {
-    const listItem = document.createElement('li')
-    listItem.classList.add("item")
-    const container = document.createElement('div')
-    listItem.appendChild(container)
-    const location = document.createElement('p')
-    location.textContent = point.commonName
+  // Get weather data 
+  fetch('/data/weather.json', { method: 'GET' })
+    .then(function(res){return res.json()})
+    .then(function(data){
 
-    const nbBikes = document.createElement('p')
-    nbBikes.textContent = point.bikesAvailable + ' bikes available!';
+      // Set Rain Percentage
+      byId("rain")
+        .textContent = data["rain"]
 
-    const button = document.createElement('button')
-    button.textContent = "View map";
-
-    [ location, nbBikes, button, ].forEach(el => {
-      container.appendChild(el)
+      // Set Polution Class
+      byId("pollution")
+        .classList
+        .add(data["pollution"])
     })
 
-    rootElement.appendChild(listItem)
+  // On clicking 'Lets Bike'
+  byId("fetch")
+    .addEventListener("click", function(e){
+      // Get location
+      
+      e.target.textContent = "Loading...";
+
+      geo(function(data){
+      var lon = data.coords.longitude,
+          lat = data.coords.latitude;
+
+      // Build endpoint
+      var endpoint = 'get_bikes?longitude='
+                   + lon 
+                   + '&latitude=' 
+                   + lat;
+      // Fetch
+      fetch(endpoint, { method: 'GET', })
+        .then(function(res){return res.json()})
+        .then(inject)
+        .then(function(){e.target.textContent = "See below"})
+    })
   })
-}
+
+
+  //Inject Bikes into Page
+  function inject (data){
+
+    var bikesPage      = byId("bikes"),
+        bikesOutput    = byId("bikes__output"),
+        hiddenClass    = "bikes--hidden",
+        bikePointClass = "bike";
+       
+        // 1. Display Bikes Page & scroll to
+        bikesPage.classList.remove(hiddenClass)
+        bikesPage.scrollIntoView({ block: "end",  behavior: 'smooth' })
+
+    // Generate bike point components
+    data.forEach(function(point){
+
+      // Google Maps Href
+      var geoHref = 'http://maps.google.com/?q='
+                  + point.latitude 
+                  + ','
+                  + point.longitude;
+
+      var bike = elt('li')
+          bike.classList.add("bike")
+
+      var info = elt('div')
+          info.classList.add("bike__info")
+
+      var location = elt('h3')
+          location.classList.add("bike__location") 
+          location.textContent = point.commonName
+
+      var count = elt('p')
+          count.classList.add("bike__count")
+          count.textContent = point.bikesAvailable + ' bikes available!';
+
+      var btn = elt('button')
+          btn.classList.add("bike__btn")
+          btn.setAttribute('href', geoHref)
+          // Add onClick handler (mimic anchor tag temporarily)
+          btn.setAttribute('onClick', 'location.href = "' + geoHref + '"')
+          btn.textContent = "View map";
+
+      // Place location & count in info container 
+      [ location, count ].forEach(el => {
+        info.appendChild(el)
+      });
+
+      // Place info & button in bike container 
+      [ info, btn ].forEach(el => {
+        bike.appendChild(el)
+      });
+
+      // Append to Bikes Output
+      bikesOutput.appendChild(bike)
+    })
+  }
+})();
